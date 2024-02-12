f3f6d1f9cad5a15478bccc7a4d9ece67a469e832
Guthrie <tegmaster2@gmail.com>
2024-02-12 17:24:07 -0500

    dynamic header/footer added

--- itis3135/scripts/header.js
+++
@@ -1,4 +0,0 @@
-const template = document.importNode();
-
-template.innerHTML = "<header><h1>Thomas Guthrie's Titanic Gopher <span class=\"divider_head\">𒅄</span> ITIS 3135</h1><nav><a href = \"./\">Home</a><span class=\"divider_nav\">𒅄</span><a href = \"introduction.html\">Introduction</a><span class=\"divider_nav\">𒅄</span><a href = \"contract.html\">Contract</a></nav></header>";
-document.body.appendChild(template.content);
