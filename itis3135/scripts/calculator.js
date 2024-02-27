1dc3d0b2c9b798c210be1b5bc476fc384da426bf
Guthrie <tegmaster2@gmail.com>
2024-02-26 19:24:11 -0500

    calculator layout

---
+++ itis3135/scripts/calculator.js
@@ -0,0 +1,13 @@
+let num1
+let num2
+let numSelect
+let operator
+function appendNum(value){
+    let num = document.getElementById("numbers").value;
+    num += value;
+    document.getElementById("numbers").value = num;
+    console.log(num);
+}
+function opperatorSelect(){
+    
+}
