3139c0a6a674fd0bd1a0902bce731ce710a362a3
TEGuthrie <tguthri2@uncc.edu>
2024-05-03 18:14:58 -0400

    fixed crappy, finalized firstscripts

--- itis3135/scripts/scripts.js
+++ itis3135/scripts/scripts.js
@@ -1,3 +1,7 @@
+let glob_size = 5;
+window.onload = function(){
+    facts();
+}
 function greetings(){
     const company = "Titanic Gopher Co"
     let name = document.getElementById("name").value;
@@ -58,7 +62,13 @@ function gonFunc(){
 }
 function size(){
     const sizes = ["miniscule", "itsy-bitsy", "tiny", "small", "average", "big", "large", "enormous", "titanic", "off the charts"];
-    let input = Math.abs(Math.ceil(document.getElementById("sizeNum")));
+    let input = parseInt(document.getElementById("sizeNum").value);
+    if(input > 10){
+        glob_size = 10;
+    }
+    else{
+        glob_size = input;
+    }
     let size;
     console.log("input: " + input);
     switch(input){
@@ -93,7 +103,51 @@ function size(){
             size = 9;
             break;
     }
-    let text = ("Your creature's size is " + sizes[size] + " compared to the titanic gopher");
+    let text = ("Your gopher's size is " + sizes[size]);
     console.log("Text: " + text);
     document.getElementById("sizeText").innerHTML = text;
+}
+function holes(){
+    const min =  Math.ceil(2);
+    const max = Math.floor(50);
+    let holes = Math.floor(Math.random() * (max - min + 1) + min);
+    document.getElementById("holes").innerHTML = ("There are " + holes + " gopher holes in your yard.");
+}
+function length(){
+    //["miniscule", "itsy-bitsy", "tiny", "small", "average", "big", "large", "enormous", "titanic", "off the charts"];
+    //                   mi  ib   tin sml avg big  lrg  eno tit
+    const multipliers = [.1, .25, .5, .75, 1, 1.2, 1.4, 1.6, 2, 3, 5];
+    const min = Math.ceil(5);
+    const max = Math.floor(14);
+    let def_size = Math.floor(Math.random() * (max - min + 1) + min);
+    console.log("glob_size:" + glob_size);
+    let size = Math.floor(def_size * multipliers[glob_size]);
+    console.log("def_size: " +def_size);
+    console.log("Size: " +size);
+    if (size <= 24){
+        document.getElementById("length").innerHTML = ("Your gopher is " + size + " inches long.");
+    }
+    else{
+        let feet = Math.floor(size/12);
+        let inches = size%12;
+        console.log("Feet: "+feet+", Inches: "+inches);
+        document.getElementById("length").innerHTML = ("Your gopher is " + feet +" feet and "+ inches +"inches long.")
+    }
+}
+function facts(){
+    const facts = [
+        "Gophers can close their lips behind their four incisors to keep dirt out of their mouths while digging.",
+        "Gophers have small eyes and ears, and no distinct neck.",
+        "Gophers have short but powerful legs that end in broad feet with large claws.",
+        "Gophers have four-inch tails that they use for navigation while moving backwards through tunnels.",
+        "Gophers have abundant whiskers that help them feel out their surroundings.",
+        "Gophers have a highly-developed sense of touch, which they rely on heavily for survival.",
+        "Gophers use pouches in their cheeks to carry food.",
+        "Gophers are known for building complex underground tunnel systems.",
+        "Gophers can travel at speeds of up to 16 miles per hour.",
+        "Gopher litters usually average 5 to 6 young. In non-irrigated areas, gophers tend to have 1 litter per year but in irrigated sites, gophers can produce up to 3 litters per year.",
+        "Gophers' predators include snakes, owls, coyotes, weasels, badgers, bobcats, and even herons."
+    ]
+    let fact = Math.floor(Math.random() * (11 - 0 + 1) + 0);
+    document.getElementById("fact").innerHTML = facts[fact];
 }
